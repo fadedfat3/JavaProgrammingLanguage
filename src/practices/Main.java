5aef864b3a96d71eb3e4e0778aa6cfcdf93c3d59
fadedfat3 <1743723571@qq.com>
2019-04-04 18:35:50 +0800

    多态

--- src/practices/Main.java
+++ src/practices/Main.java
@@ -3,11 +3,40 @@ import practices.Test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Test t = new Test();
-        t.sort();
-        System.out.println(t.toString());
+        // write your code here
+        Person p1 = new Person();
+        Student p2 = new Student();
+        p1.printInfo();
+        p2.printInfo();
+        new GStudent().printInfo();
+        p2.f();
+        new GStudent().f();
+    }
+}
+    class GStudent extends Student{
+        @Override
+        public String getInfo(){
+            return "Good Student";
+        }
+    }
+    class Student extends Person{
+
+        public String getInfo(){
+            return "Student";
+        }
+        public void f(){
+            System.out.println(getInfo());
+        }
+    }
+
+    class Person {
+        private String getInfo(){
+            return "Person";
+        }
+        public void printInfo(){
+            System.out.println(getInfo());
+        }
     }
 
 
-}
+
--- src/practices/Test.java
+++ src/practices/Test.java
@@ -3,21 +3,7 @@ package practices;
 import java.util.Arrays;
 
 public class Test {
-    private int a = 0;
-    private String[] str = {"abc", "efg", "bsfe", "vnfjef"};
-    public void add(){
-        ++a;
-    }
-    public int get(){
-        return a;
-    }
 
-    public void sort(){
-        Arrays.sort(str);
 
-    }
-    public String toString(){
-        return Arrays.toString(str);
-    }
 
 }
